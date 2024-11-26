69d9fa8de2d222dbab7de60f12811481b3c63029
unknown <hugo.houdain2@gmail.com>
2024-11-26 16:58:22 +0100

    feat/ico

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+
+  public stackoverflowUrl: string = 'https://stackoverflow.com/';
+  public linkedinUrl: string = 'https://www.linkedin.com/';
+  public fiverrUrl:string = 'https://www.fiverr.com/';
+  public facebookUrl:string = 'https://www.facebook.com/';
+  public twitterUrl:string = 'https://twitter.com/';
+  public githubUrl:string = 'https://github.com/';
+  public googleUrl:string = 'https://plus.google.com/';
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
