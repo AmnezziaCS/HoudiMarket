cc534f7c990aebb542696460a6efc65600380dcb
Mathys Visticot <mathys.vst@gmail.com>
2024-12-10 16:24:12 +0100

    feat(newsletter): add the newsletter field

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,23 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit{
+export class FooterComponent implements OnInit {
 
   public stackoverflowUrl: string = 'https://stackoverflow.com/';
+  public facebookUrl: string = 'https://www.facebook.com/';
+  public googleUrl: string = 'https://plus.google.com/';
   public linkedinUrl: string = 'https://www.linkedin.com/';
-  public fiverrUrl:string = 'https://www.fiverr.com/';
-  public facebookUrl:string = 'https://www.facebook.com/';
-  public twitterUrl:string = 'https://twitter.com/';
-  public githubUrl:string = 'https://github.com/';
-  public googleUrl:string = 'https://plus.google.com/';
+
+  public errorMessage: string | null = null;
+  public successMessage: string | null = null;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit(form: any) {
+    if (!form.valid) {
+      this.errorMessage = 'Il faut une addresse email pour s\'inscrire à la newsletter';
+      this.successMessage = null;
+      console.log('Invalid form');
+    } else {
+      this.errorMessage = null;
+      this.successMessage = 'Inscrit à la newsletter';
+      console.log(`Valid form with email: ${form.value.email}`);
+    }
+  }
 }
